895c1e0ceb4e93668efb385103dc9f73fffb8ec3
dzungpnguyen <thienhoangphuongdung@gmail.com>
2022-11-23 22:20:30 +0100

    Read CSV (continue)

--- airport/src/main/scala/application/main.scala
+++ airport/src/main/scala/application/main.scala
@@ -1,95 +1,143 @@
 package application
 
 import model.Airport
+import model.Country
+import model.Runway
 
-object Show_airport {
+object MyApplication {
   def main(args: Array[String]) = {
 
     // airports
-    val lst_airports = scala.io.Source.fromFile("src/resources/airports.csv").getLines()
-    getAllAirports(lst_airports)
+    val lst_airports = scala.io.Source.fromFile("src/resources/airports.csv").getLines().iterator
+      .map(_.split(","))
+      // keep valid row
+      .filter(arr => arr.length == 18)
+      // generate object
+      .map(obj => Airport.apply(obj(0),
+                                obj(1),
+                                obj(2),
+                                obj(3),
+                                obj(4).toFloatOption,
+                                obj(5).toFloatOption,
+                                obj(6).toIntOption,
+                                obj(7),
+                                obj(8),
+                                obj(9),
+                                obj(10),
+                                obj(11),
+                                obj(12),
+                                obj(13),
+                                obj(14),
+                                obj(15),
+                                obj(16),
+                                obj(17)
+                              )
+            )
     // lst_airports.foreach(println)
 
-
     // countries
-    val lst_countries = scala.io.Source.fromFile("src/resources/countries.csv").getLines()
-    getAllCountries(lst_countries)
-
+    val lst_countries = scala.io.Source.fromFile("src/resources/countries.csv").getLines().iterator
+      .map(_.split(","))
+      .filter(arr => arr.length == 6)
+      .map(obj => Country.apply(obj(0),
+                                obj(1),
+                                obj(2),
+                                obj(3),
+                                obj(4),
+                                obj(5),
+                              )
+            )
 
     // runways
-    val lst_runway = scala.io.Source.fromFile("src/resources/runways.csv").getLines()
-    getAllRunways(lst_runway)
-  }
+    val lst_runways = scala.io.Source.fromFile("src/resources/runways.csv").getLines().iterator
+      .map(_.split(","))
+      .filter(arr => arr.length == 20)
+      .map(obj => Runway.apply(obj(0),
+                                obj(1),
+                                obj(2),
+                                obj(3).toIntOption,
+                                obj(4).toIntOption,
+                                obj(5),
+                                obj(6).toBooleanOption,
+                                obj(7).toBooleanOption,
+                                obj(8),
+                                obj(9).toFloatOption,
+                                obj(10).toFloatOption,
+                                obj(11).toIntOption,
+                                obj(12).toFloatOption,
+                                obj(13).toFloatOption,
+                                obj(14),
+                                obj(15).toFloatOption,
+                                obj(16).toFloatOption,
+                                obj(17).toIntOption,
+                                obj(18).toFloatOption,
+                                obj(19).toFloatOption
+                              )
+            )
 
-  def getAllAirports(lst: Iterator[String]) {
-    // split row by ","
-    lst.map(_.split(","))
-        // keep valid row
-        .filter(arr => arr.length == 18)
-        // generate object
-        .map(obj => Airport.apply(obj(0),
-                                  obj(1),
-                                  obj(2),
-                                  obj(3),
-                                  obj(4).toFloatOption,
-                                  obj(5).toFloatOption,
-                                  obj(6).toIntOption,
-                                  obj(7),
-                                  obj(8),
-                                  obj(9),
-                                  obj(10),
-                                  obj(11),
-                                  obj(12),
-                                  obj(13),
-                                  obj(14),
-                                  obj(15),
-                                  obj(16),
-                                  obj(17)))
-  }
-
-  def getAllCountries(lst: Iterator[String]) {
-    // split row by ","
-    lst.map(_.split(","))
-        // keep valid row
-        .filter(arr => arr.length == 6)
-        // generate object
-        .map(obj => Airport.apply(obj(0),
-                                  obj(1),
-                                  obj(2),
-                                  obj(3),
-                                  obj(4),
-                                  obj(5),
-                                  )
-              )
-  }
-  
-  def getAllRunways(lst: Iterator[String]) {
-    // split row by ","
-    lst.map(_.split(","))
-        // keep valid row
-        .filter(arr => arr.length == 20)
-        // generate object
-        .map(obj => Airport.apply(obj(0),
-                                  obj(1),
-                                  obj(2),
-                                  obj(3).toIntOption,
-                                  obj(4).toIntOption,
-                                  obj(5),
-                                  obj(6).toBooleanOption,
-                                  obj(7).toBooleanOption,
-                                  obj(8),
-                                  obj(9).toIntOption,
-                                  obj(10).toIntOption,
-                                  obj(11),
-                                  obj(12).toIntOption,
-                                  obj(13).toIntOption,
-                                  obj(14).toIntOption,
-                                  obj(15).toIntOption,
-                                  obj(16).toIntOption,
-                                  obj(17),
-                                  obj(18),
-                                  obj(19).toIntOption
-                                )
-              )
   }
 }
+
+//   def getAllAirports(lst: Iterator[String]) {
+//     lst.map(_.split(","))
+//         .filter(arr => arr.length == 18)
+//         .map(obj => Airport.apply(obj(0),
+//                                   obj(1),
+//                                   obj(2),
+//                                   obj(3),
+//                                   obj(4).toFloatOption,
+//                                   obj(5).toFloatOption,
+//                                   obj(6).toIntOption,
+//                                   obj(7),
+//                                   obj(8),
+//                                   obj(9),
+//                                   obj(10),
+//                                   obj(11),
+//                                   obj(12),
+//                                   obj(13),
+//                                   obj(14),
+//                                   obj(15),
+//                                   obj(16),
+//                                   obj(17)))
+//   }
+
+//   def getAllCountries(lst: Iterator[String]) {
+//     lst.map(_.split(","))
+//         .filter(arr => arr.length == 6)
+//         .map(obj => Country.apply(obj(0),
+//                                   obj(1),
+//                                   obj(2),
+//                                   obj(3),
+//                                   obj(4),
+//                                   obj(5),
+//                                   )
+//               )
+//   }
+  
+//   def getAllRunways(lst: Iterator[String]) {
+//     lst.map(_.split(","))
+//         .filter(arr => arr.length == 20)
+//         .map(obj => Runway.apply(obj(0),
+//                                   obj(1),
+//                                   obj(2),
+//                                   obj(3).toIntOption,
+//                                   obj(4).toIntOption,
+//                                   obj(5),
+//                                   obj(6).toBooleanOption,
+//                                   obj(7).toBooleanOption,
+//                                   obj(8),
+//                                   obj(9).toFloatOption,
+//                                   obj(10).toFloatOption,
+//                                   obj(11).toIntOption,
+//                                   obj(12).toFloatOption,
+//                                   obj(13).toFloatOption,
+//                                   obj(14),
+//                                   obj(15).toFloatOption,
+//                                   obj(16).toFloatOption,
+//                                   obj(17).toIntOption,
+//                                   obj(18).toFloatOption,
+//                                   obj(19).toFloatOption
+//                                 )
+//               )
+//   }
+// }
